e53267b5b48efae12370c4965415e955ad8ba939
盧志昇 <billy@luzhishengde-MacBook-Pro.local>
2019-03-21 21:21:16 +0800

    udpate

--- app/src/main/java/com/billy/petsadoption/fragment/CatFragment.kt
+++ app/src/main/java/com/billy/petsadoption/fragment/CatFragment.kt
@@ -8,18 +8,44 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.billy.petsadoption.PaginationScrollListener
 import com.billy.petsadoption.PetViewModel
 import com.billy.petsadoption.R
 import com.billy.petsadoption.databinding.FragmentPetsBinding
 
+
+
 class CatFragment: Fragment() {
     private val TAG = javaClass.simpleName
+
+    private val isLoading = false
+    // 如果当前页面是最后一页（页面加载后分页将停止）
+    private val isLastPage = false
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         var binding = DataBindingUtil.inflate<FragmentPetsBinding>(inflater,
             R.layout.fragment_pets, container, false)
         Log.i(TAG, TAG)
-        binding.petsRecyclerView.layoutManager = GridLayoutManager(container!!.context, 2)
+        var layoutManager = GridLayoutManager(container!!.context, 2)
+        binding.petsRecyclerView.layoutManager = layoutManager
+        binding.petsRecyclerView.addOnScrollListener(object : PaginationScrollListener(layoutManager) {
+            override fun loadMoreItems() {
+
+            }
+
+            override val isLastPage: Boolean
+                get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
+
+            override val isLoading: Boolean
+                get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
+
+            override val totalPageCount: Int
+                get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
+
+
+        })
+
+
 
         PetViewModel(container.context, "cat", binding)
 
